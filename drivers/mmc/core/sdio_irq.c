58819224cf7349917dc7121a1d19507aeb91e148
ptmr3 <ptmr323@gmail.com>
2013-04-10 10:23:05 -0600

    Linux 3.0.33

--- drivers/mmc/core/sdio_irq.c
+++ drivers/mmc/core/sdio_irq.c
@@ -104,7 +104,8 @@ static int sdio_irq_thread(void *_host)
 		ret = __mmc_claim_host(host, &host->sdio_irq_thread_abort);
 		if (ret)
 			break;
-		ret = process_sdio_pending_irqs(host->card);
+		ret = process_sdio_pending_irqs(host);
+		host->sdio_irq_pending = false;
 		mmc_release_host(host);
 
 		/*
