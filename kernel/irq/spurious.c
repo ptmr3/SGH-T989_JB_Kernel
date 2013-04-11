e93c89af2ca727c834a0b7b28094a385c77be9bf
ptmr3 <ptmr323@gmail.com>
2013-04-11 12:56:02 -0600

    Linux 3.0.67

--- kernel/irq/spurious.c
+++ kernel/irq/spurious.c
@@ -102,6 +102,7 @@ static int try_one_irq(int irq, struct irq_desc *desc, bool force)
 	do {
 		if (handle_irq_event(desc) == IRQ_HANDLED)
 			ret = IRQ_HANDLED;
+		/* Make sure that there is still a valid action */
 		action = desc->action;
 	} while ((desc->istate & IRQS_PENDING) && action);
 	desc->istate &= ~IRQS_POLL_INPROGRESS;
