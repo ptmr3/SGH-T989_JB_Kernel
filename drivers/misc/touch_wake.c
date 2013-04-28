ca2b6eef65119f91a81bd06ed8ad0a4a7cb6f5ce
ptmr3 <ptmr323@gmail.com>
2013-04-28 14:18:52 -0600

    Untracked Expirimentals

--- drivers/misc/touch_wake.c
+++ drivers/misc/touch_wake.c
@@ -31,8 +31,6 @@ static bool timed_out = true;
 
 static unsigned int touchoff_delay = 45000;
 
-static const unsigned int presspower_delay = 100;
-
 static void touchwake_touchoff(struct work_struct * touchoff_work);
 
 static DECLARE_DELAYED_WORK(touchoff_work, touchwake_touchoff);
@@ -52,6 +50,10 @@ static struct timeval last_powerkeypress;
 #define TOUCHWAKE_VERSION 1
 
 #define TIME_LONGPRESS 500
+
+#define POWERPRESS_DELAY 100
+
+#define POWERPRESS_TIMEOUT 1000
 
 static void touchwake_disable_touch(void)
 {
@@ -146,11 +148,13 @@ static void press_powerkey(struct work_struct * presspower_work)
 {
     input_event(powerkey_device, EV_KEY, KEY_POWER, 1);
     input_event(powerkey_device, EV_SYN, 0, 0);
-    msleep(presspower_delay);
+    msleep(POWERPRESS_DELAY);
 
     input_event(powerkey_device, EV_KEY, KEY_POWER, 0);
     input_event(powerkey_device, EV_SYN, 0, 0);
-    msleep(presspower_delay);
+    msleep(POWERPRESS_DELAY);
+
+    msleep(POWERPRESS_TIMEOUT);
 
     mutex_unlock(&lock);
 
@@ -169,7 +173,7 @@ static ssize_t touchwake_status_write(struct device * dev, struct device_attribu
     if(sscanf(buf, "%u\n", &data) == 1) 
 	{
 	    pr_devel("%s: %u \n", __FUNCTION__, data);
-
+	    
 	    if (data == 1) 
 		{
 		    pr_info("%s: TOUCHWAKE function enabled\n", __FUNCTION__);
@@ -254,6 +258,14 @@ void proximity_detected(void)
 }
 EXPORT_SYMBOL(proximity_detected);
 
+void proximity_off(void)
+{   
+    timed_out = true;
+
+    return;
+}
+EXPORT_SYMBOL(proximity_off);
+
 void powerkey_pressed(void)
 {   
     do_gettimeofday(&last_powerkeypress);
@@ -299,7 +311,7 @@ void set_powerkeydev(struct input_dev * input_device)
 
     return;
 }
-//EXPORT_SYMBOL(set_powerkeydev);
+EXPORT_SYMBOL(set_powerkeydev);
 
 bool device_is_suspended(void)
 {   
