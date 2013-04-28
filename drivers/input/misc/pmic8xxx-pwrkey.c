3a30740214c7b188296bfb8e46e17d15bdca9248
ptmr3 <ptmr323@gmail.com>
2013-04-28 14:19:22 -0600

    Revert "Revert "Exp edit""
    This reverts commit ef31c49cd7d832675ab019be973c8a579ce49890.

--- drivers/input/misc/pmic8xxx-pwrkey.c
+++ drivers/input/misc/pmic8xxx-pwrkey.c
@@ -27,6 +27,10 @@
 #include <linux/string.h>
 #include <linux/delay.h>
 
+#ifdef CONFIG_TOUCH_WAKE
+#include <linux/touch_wake.h>
+#endif
+
 #define PON_CNTL_1 0x1C
 #define PON_CNTL_PULL_UP BIT(7)
 #define PON_CNTL_TRIG_DELAY_MASK (0x7)
@@ -195,6 +199,11 @@ static int __devinit pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 	pwrkey->pwr = pwr;
 
 	platform_set_drvdata(pdev, pwrkey);
+
+#ifdef CONFIG_TOUCH_WAKE
+  touchwake_setdev(pwr);
+  printk(KERN_INFO "[touchwake]: set device %s\n", pwr->name);
+#endif
 
 	err = request_any_context_irq(key_press_irq, pwrkey_press_irq,
 		IRQF_TRIGGER_RISING, "pmic8xxx_pwrkey_press", pwrkey);
